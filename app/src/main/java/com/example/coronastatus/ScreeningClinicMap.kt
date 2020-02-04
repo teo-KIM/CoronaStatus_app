50896881a195f4021436abadc7b13784c95162bf
TEO <ted243@naver.com>
2020-02-04 13:59:22 +0900

    [update] ScreeningClinicMap.kt borad_btn 클릭시 MainActivity로 화면전환 완료

--- app/src/main/java/com/example/coronastatus/ScreeningClinicMap.kt
+++ app/src/main/java/com/example/coronastatus/ScreeningClinicMap.kt
@@ -1,12 +1,19 @@
 package com.example.coronastatus
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class ScreeningClinicMap : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.screening_clinic_map)
+
+        board_btn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java);
+            startActivity(intent)
+        }
     }
 }
