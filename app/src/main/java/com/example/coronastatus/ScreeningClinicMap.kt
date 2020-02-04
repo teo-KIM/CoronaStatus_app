ad2186145645aff5d4e929c3a57c5cc6ea847000
TEO <ted243@naver.com>
2020-02-04 13:43:38 +0900

    [update] ScreeningClinicMap.kt와 screening_clinic_map.xml 연결

--- app/src/main/java/com/example/coronastatus/ScreeningClinicMap.kt
+++ app/src/main/java/com/example/coronastatus/ScreeningClinicMap.kt
@@ -1,2 +1,12 @@
 package com.example.coronastatus
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ScreeningClinicMap : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.screening_clinic_map)
+    }
+}
