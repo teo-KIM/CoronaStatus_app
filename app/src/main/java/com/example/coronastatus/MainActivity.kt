f78c39d00d06385edf62945a3fcf2b341c177522
TEO <ted243@naver.com>
2020-02-06 13:32:48 +0900

    [add] Manifest.xml network state 확인하는 permission 추가

--- app/src/main/java/com/example/coronastatus/MainActivity.kt
+++ app/src/main/java/com/example/coronastatus/MainActivity.kt
@@ -18,6 +18,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
         fetchJson()
 
         map_btn.setOnClickListener {
