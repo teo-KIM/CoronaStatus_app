6d37ac9a461e87a48c7b2b8cfb96bdce550fc94d
TEO <ted243@naver.com>
2020-02-06 16:12:20 +0900

    [update] MyFirebaseMessageingService.kt 채널 ID, name 수정, notification builder에서 아이콘, 글자 색 변경 완료

--- app/src/main/java/com/example/coronastatus/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/coronastatus/MyFirebaseMessagingService.kt
@@ -8,16 +8,21 @@ import android.content.Intent
 import android.media.RingtoneManager
 import android.util.Log
 import androidx.core.app.NotificationCompat
-import androidx.core.content.ContextCompat.getSystemService
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import android.app.NotificationChannel
+import android.graphics.Color
+
+import android.os.Build
+
+
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
-    //푸시 메시지를 받았을 때 동작하는 서비스 설정
+    //firebase에서 메시지를 받으면 디바이스에 알람이 뜨게 하는 class
     private val TAG: String = MainActivity::class.java.simpleName
 
     override fun onNewToken(token: String) {
-        Log.d("New_Token", token)
+//        Log.d("New_Token", token)
     }
 
     @SuppressLint("LongLogTag")
@@ -35,13 +40,19 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     private fun sendNotification(remoteMessage: RemoteMessage) {
         //추출해온 remoteMessage에서 body, title을 추출해서 디바이스로 알림을 전송한다.
+
         val intent = Intent(this, MainActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
 
         val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+
+        val channelId : String = getString(R.string.default_notification_channel_id)
         val soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-        val notificationBuilder = NotificationCompat.Builder(this, "Notification")
-            .setSmallIcon(R.mipmap.ic_launcher)
+        val color = Color.RED
+
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.virus)
+            .setColor(color)
             .setContentTitle(remoteMessage.notification?.title)
             .setContentText(remoteMessage.notification?.body)
             .setAutoCancel(true)
@@ -50,6 +61,15 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
         val notificationManager =
             getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelName = getString(R.string.default_notification_channel_name)
+            val channel =
+                NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH) as NotificationChannel
+            notificationManager.createNotificationChannel(channel)
+        }
+
         notificationManager.notify(0, notificationBuilder.build())
 
     }
