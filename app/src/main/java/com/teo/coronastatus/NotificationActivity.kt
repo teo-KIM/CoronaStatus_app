d2b00c1dc35b842346c9ca94adfd30689c57afb1
TEO <ted243@naver.com>
2020-02-21 17:43:36 +0900

    [update] NotificationActivity.kt notification DB에서 데이터 가져오는 메소드 추가. 아직 listview와 연결되지 않음

--- app/src/main/java/com/teo/coronastatus/NotificationActivity.kt
+++ app/src/main/java/com/teo/coronastatus/NotificationActivity.kt
@@ -5,23 +5,34 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
 import android.widget.BaseAdapter
 import android.widget.ListView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
+import java.net.URL
 
-private val TAG: String = MainActivity::class.java.simpleName
+private val TAG: String = NotificationActivity::class.java.simpleName
 
 class NotificationActivity : AppCompatActivity() {
+
+    //db에서 가져올 데이터를 담을 배열
+    //getNotification()에서 사용한다.
+    val dateList = mutableListOf<String>()
+    val contentList = mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notification)
 
-        val listView = findViewById<ListView>(R.id.notification_lv)
+        //알람 날짜와 내용을 db에서부터 가져온다.
+        getNotification()
 
         //어답터 설정
-        listView.adapter = MyCustomAdapter(this)
+        val listView = findViewById<ListView>(R.id.notification_lv)
+        listView.adapter = MyCustomAdapter(this, dateList, contentList)
 
         //아이템 클릭 리스너
         /*listView.onItemClickListener =
@@ -33,30 +44,66 @@ class NotificationActivity : AppCompatActivity() {
     }
 
     fun getNotification() {
+        val url = URL(getString(R.string.notification))
+        val request = Request.Builder().url(url).build()
+        val client = OkHttpClient()
 
+        client.newCall(request).enqueue(object : Callback {
+            override fun onResponse(call: Call, response: Response?) {
+                //응답이 있을 경우 call은 무조건 null이 아니므로 ?를 쓰지 않는다.
+                //json 형식으로 받아온 데이터를 until_yesterday, today 배열에 저장하고 해당하는 textview에 값을 넣어준다.
+                //("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+                val body = response?.body()?.string()
+//                Log.d(TAG, "Success to execute request! : $body")
+
+                val jObject = JSONObject(body)
+                val jArray = jObject.getJSONArray("notification")
+
+                for (i in 0 until jArray.length()) {
+                    val obj = jArray.getJSONObject(i)
+
+                    val date = obj.getString("date")
+                    //date = db에 저장된 알람 보낸 날짜 / 시간
+
+                    val content = obj.getString("content")
+                    //content = db에 저장된 알람 내용
+
+                    dateList.add(date)
+                    contentList.add(content)
+
+//                    Log.d(TAG, "dateList[$i] : "+ dateList[i])
+//                    Log.d(TAG, "contentList($i) : contentList")
+                }
+            }
+
+            override fun onFailure(call: Call?, e: IOException?) {
+                //("not implemented") //To change body of created functions use File | Settings | File Templates.
+                println("Failed to get notification from NotificationActivity.kt")
+            }
+        })
     }
-
 
     override fun onResume() {
         super.onResume()
-        //페이지 이동 시 넘어가는 애니메이션 삭제
+        //액티비티 이동 시 넘어가는 애니메이션 삭제
         overridePendingTransition(0, 0);
     }
 
-    private class MyCustomAdapter(context: Context) : BaseAdapter() {
+    private class MyCustomAdapter(context: Context, dateList : MutableList<String>, contentList : MutableList<String>) : BaseAdapter() {
         private val mContext: Context
 
-        //데이터 어레이
-        private val names = arrayListOf<String>(
-            "이순신", "강감찬", "연개소문", "김유신", "을지문덕", "링컨", "워싱턴", "태종", "박혁거세", "광개토대왕"
-        )
+        //listView에 표현 해 줄 알람 온 날짜 리스트
+        private val dates = dateList
+        //listView에 표현 해 줄 알람 내용 리스트
+        private val contents = contentList
 
         init {
             mContext = context
         }
 
         override fun getCount(): Int {
-            return names.size
+            return dates.size
         }
 
         override fun getItemId(position: Int): Long {
@@ -64,7 +111,7 @@ class NotificationActivity : AppCompatActivity() {
         }
 
         override fun getItem(position: Int): Any {
-            val selectItem = names.get(position)
+            val selectItem = dates.get(position)
             return selectItem
         }
 
@@ -72,10 +119,10 @@ class NotificationActivity : AppCompatActivity() {
             val layoutInflater = LayoutInflater.from(mContext)
             val rowMain = layoutInflater.inflate(R.layout.item_notification, viewGroup, false)
 
-            val nameTextView = rowMain.findViewById<TextView>(R.id.date_tv)
-            nameTextView.text = names.get(position)
-            val positionTextView = rowMain.findViewById<TextView>(R.id.content_tv)
-            positionTextView.text = "순서: " + position
+            val datesTv = rowMain.findViewById<TextView>(R.id.date_tv)
+            datesTv.text = dates.get(position)
+            val contentsTv = rowMain.findViewById<TextView>(R.id.content_tv)
+            contentsTv.text = contents.get(position)
 
             return rowMain
         }
