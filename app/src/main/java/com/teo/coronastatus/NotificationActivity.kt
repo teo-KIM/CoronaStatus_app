888fbec98e8898f3329aee21cea4bc2b2c8f7e92
TEO <ted243@naver.com>
2020-02-21 14:50:27 +0900

    [add] NotificationActivity.kt 및 class 생성

---
+++ app/src/main/java/com/teo/coronastatus/NotificationActivity.kt
@@ -0,0 +1,9 @@
+package com.teo.coronastatus
+
+import androidx.appcompat.app.AppCompatActivity
+
+private val TAG: String = MainActivity::class.java.simpleName
+
+class NotificationActivity: AppCompatActivity() {
+
+}
